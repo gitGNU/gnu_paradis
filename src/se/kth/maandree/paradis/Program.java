5077db36f2ae72602b77bd9164bec570d31c5dea
Mattias Andrée <maandree@operamail.com>
2012-06-06 14:30:26 +0200

    main + legal crap

---
+++ src/se/kth/maandree/paradis/Program.java
@@ -0,0 +1,39 @@
+/**
+ *  Paradis — Ever growing network for parallell and distributed computing.
+ *  Copyright © 2012  Mattias Andrée
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.kth.maandree.paradis;
+
+
+/**
+ * The is the main class of the program
+ * 
+ * @author  Mattias Andrée, <a href="mailto:maandree@kth.se">maandree@kth.se</a>
+ */
+public class Program
+{
+    /**
+     * This is the main entry point of the program
+     * 
+     * @param  args  Startip arguments, unused
+     */
+    public static void main(final String... args)
+    {
+	// Start program here
+    }
+    
+}
+
