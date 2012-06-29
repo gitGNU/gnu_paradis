6fe9d367f7b639ceae84d72dfaac80a19a38a055
Mattias Andrée <maandree@operamail.com>
2012-06-29 07:19:59 +0200

    clean script converts tabs to spaces

--- clean.sh
+++ clean.sh
@@ -6,3 +6,5 @@ rm $(find . | grep '~$')       2>  /dev/null
 rm $(find . | grep '.class$')  2>  /dev/null
 rm $(find . | egrep '/(.|)#')  2>  /dev/null
 
+expand $(find src/se/kth/maandree/paradis | grep '.java$')
+git add $(find src/se/kth/maandree/paradis | grep '.java$')
