e5d2f64c9e6d2abb886c49f44c5400353179a995
Mattias Andrée <maandree@operamail.com>
2012-07-07 14:57:05 +0200

    moving classes

--- src/se/kth/maandree/paradis/pacman/PacmanUpgrade.java
+++ src/se/kth/maandree/paradis/pacman/PacmanUpgrade.java
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.kth.maandree.paradis.plugin;
+package se.kth.maandree.paradis.pacman;
 import se.kth.maandree.paradis.*;
 
 import java.util.*;
