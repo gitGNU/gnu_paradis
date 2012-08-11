7b1dffbb02a8db8dd4144b7b15d85b5dbf39333a
Mattias Andrée <mattias@localhost.localdomain>
2012-08-11 13:40:07 +0200

    typo

--- src/org/nongnu/paradis/pacman/Pacman.java
+++ src/org/nongnu/paradis/pacman/Pacman.java
@@ -187,7 +187,7 @@ public class Pacman
     /** Do not return up to date packages
      */ public static final String QUERY_UPGRADE = "--upgrade"; // -u
     
-    // TODO implement equivalent to Arch:s pacman -Qo
+    // TODO implement equivalent to Arch's pacman -Qo
     /** Finds the owner package of an installed file
      */ public static final String DATABASE_OWNER = "--owner"; // -o
     
