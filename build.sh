40bf5e16f30460a3fdbaaa3aa998a473db4d794e
Mattias Andrée <maandree@operamail.com>
2013-04-07 14:25:41 +0200

    cont. + gnuallpermissive instead of public domain
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-# This script is put in Public Domain
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+# 
+# [GNU All Permissive License]
+# 
 # Year: 2012, 2013
 # Author: Mattias Andrée (maandree@member.fsf.org)
 
